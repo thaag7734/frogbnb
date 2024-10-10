abca977ba506432603596d9cada189ae050b4805
thaag7734 <thaag7734@gmail.com>
2024-10-10 19:52:38 -0400

    add basic unstyled navigation component

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { IoPersonCircleSharp } from 'react-icons/io5';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <button>
+        <IoPersonCircleSharp />
+      </button>
+      <ul className="profile-dropdown">
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default ProfileButton;
